3fe56c3c2b7bc25920eaedeb0ba3721c15b0b699
riddle-master-ruben <119084862+riddle-master-ruben@users.noreply.github.com>
2024-09-28 09:20:50 -0600

    Redo project structure

---
+++ client/src/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+        <p><Link to="/">Back to Home</Link></p>  {/* Link back to home page */}
+      </form>
+    </div>
+  );
+}
+
+export default Login;
