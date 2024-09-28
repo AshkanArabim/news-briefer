0423d6b0a7ad356d3081c732b44cbb403810f9aa
Alexander Karl <atkarl255@gmail.com>
2024-09-28 12:08:43 -0600

    Edited the system instructions further to hopefully better support multiple articles

--- server/src/ai_test.py
+++ server/src/ai_test.py
@@ -3,33 +3,34 @@ import vertexai
 from vertexai.generative_models import GenerativeModel
 import parse_rss
 
-#Load the API key from the config file
+# Load the API key from the config file in your directory
+# KEEP API FILE OUTSIDE OF REPO
 configpath = '/home/alex/Documents/Personal Projects/Hackathon/API Key/calcium-petal-437012-u5-dc45b7195f9e.json'
 with open(configpath, 'r') as f:
     config = json.load(f)
 API_KEY = config
 
-#Update with your actual project ID
+# Update with your actual project ID
 PROJECT_ID = "calcium-petal-437012-u5"
 
-#Initialize the Vertex AI
+# Initialize the Vertex AI
 vertexai.init(project=PROJECT_ID, location="us-central1")
 
-#Load the generative model
+# Load the generative model
 model = GenerativeModel(
     model_name = "gemini-1.5-flash-002",
     system_instruction = [
         "You are a news briefing assistant",
-        "Your task is to take a news article as an input and generate a short daily news brief per article given that captures the main points of the article.",
-        "Keep the summary short, around 3 sentences, and focus on the key information.",
+        "Your task is to take one or more articles as an input and generate a short daily news brief per article given that captures the main points of said article.",
+        "Keep the summary short, around 3-4 sentences, and focus on the key information provided.",
         "The summary should be clear and presented in an informative and engaging tone for a morning briefing.",
-        "Avoid including unnecessary details and focus on the main points of the article. Do not say anything an AI would say. Only provide the summary",
+        "Avoid including unnecessary details and focus on the main points of the articles. Do not say anything an AI would say. Only provide the summary",
         "Maintain a non-biased perspective and avoid introducing personal opinions or interpretations.",
         "Depending on the user's preference, the summary can be generated in English or another language."
-]
+    ]
 )
 
-#Generate the news brief
+# Generate the news brief
 if __name__ == "__main__":
     content = parse_rss.get_top5_articles("https://www.cbsnews.com/latest/rss/politics")
     output = model.generate_content(content)
