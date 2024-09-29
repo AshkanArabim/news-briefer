5b6ec1feb3ee112391cd96107067b8b15425e40a
AshkanArabim <ashkan.arabim@gmail.com>
2024-09-29 09:32:00 -0600

    add headlines to UI

--- client/src/Feed.js
+++ client/src/Feed.js
@@ -3,16 +3,39 @@ import { Link } from "react-router-dom";
 import "./Feed.css";
 import { BACKEND_URL } from "./vars";
 import store from "./store";
+import { useEffect } from "react";
 
 function Feed({ translations }) {
+	const [headers, setHeaders] = React.useState([]);
+
+	useEffect(() => {
+		const fetchHeaders = async () => {
+			try {
+				const response = await fetch(`${BACKEND_URL}/get-headers/${store.getState().user.token}`, {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				});
+				const data = await response.json();
+				const newHeaders = data.headlines
+				setHeaders(newHeaders);
+			} catch (error) {
+				console.error("Error fetching headers:", error);
+			}
+		};
+
+		fetchHeaders();
+	}, []);
+
 	// Function to show an alert when the icon is clicked
 	const handleIconClick = async () => {
 		console.log("Speaker icon clicked!");
 
 		const audio_blob = await fetch(`${BACKEND_URL}/get-audio/${store.getState().user.token}`);
-    const audioUrl = URL.createObjectURL(await audio_blob.blob());
-    const audio = new Audio(audioUrl);
-    audio.play();
+		const audioUrl = URL.createObjectURL(await audio_blob.blob());
+		const audio = new Audio(audioUrl);
+		audio.play();
 	};
 
 	return (
@@ -39,6 +62,15 @@ function Feed({ translations }) {
 					className="speaker-icon"
 					onClick={handleIconClick} // Trigger alert on click
 				/>
+			</div>
+			<div>
+				<ul>
+					{headers.map((headline) => (
+						<li>
+							<h3>{headline}</h3>
+						</li>
+					))}
+				</ul>
 			</div>
 		</div>
 	);
