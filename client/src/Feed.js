033b74f42bbf7548f1d93e7b39cfe5121ca659c1
AshkanArabim <ashkan.arabim@gmail.com>
2024-09-29 06:44:48 -0600

    add audio playback to front end

--- client/src/Feed.js
+++ client/src/Feed.js
@@ -1,41 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Feed.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Feed.css";
+import { BACKEND_URL } from "./vars";
+import store from "./store";
 
 function Feed({ translations }) {
-  // Function to show an alert when the icon is clicked
-  const handleIconClick = () => {
-    alert('Speaker icon clicked!');
-  };
+	// Function to show an alert when the icon is clicked
+	const handleIconClick = async () => {
+		console.log("Speaker icon clicked!");
 
-  return (
-    <div className="feed-container">
-      {/* Home button at the top center */}
-      <div className="home-button-container">
-        <Link to="/">
-          <button className="home-button">{translations ? translations.homeTitle : 'Home'}</button>
-        </Link>
-      </div>
-      
-      {/* Feed page content */}
-      <h1 className="feed-title">{translations ? translations.feedButton : 'Feed Page'}</h1>
+		const audio_blob = await fetch(`${BACKEND_URL}/get-audio/${store.getState().user.token}`);
+    const audioUrl = URL.createObjectURL(await audio_blob.blob());
+    const audio = new Audio(audioUrl);
+    audio.play();
+	};
 
-      {/* Use the translations prop for the paragraph content */}
-      <div className="feed-subtitle-container">
-        <p className="feed-subtitle">
-          {translations ? translations.feedContent : 'Here is what is happening today:'}
-        </p>
-        {/* Insert the speaker icon next to the text */}
-        <img
-          src="/speaker.png"  // Corrected path for the public folder
-          alt="Speaker Icon"
-          className="speaker-icon"
-          onClick={handleIconClick}  // Trigger alert on click
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="feed-container">
+			{/* Home button at the top center */}
+			<div className="home-button-container">
+				<Link to="/">
+					<button className="home-button">{translations ? translations.homeTitle : "Home"}</button>
+				</Link>
+			</div>
+
+			{/* Feed page content */}
+			<h1 className="feed-title">{translations ? translations.feedButton : "Feed Page"}</h1>
+
+			{/* Use the translations prop for the paragraph content */}
+			<div className="feed-subtitle-container">
+				<p className="feed-subtitle">
+					{translations ? translations.feedContent : "Here is what is happening today:"}
+				</p>
+				{/* Insert the speaker icon next to the text */}
+				<img
+					src="/speaker.png" // Corrected path for the public folder
+					alt="Speaker Icon"
+					className="speaker-icon"
+					onClick={handleIconClick} // Trigger alert on click
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default Feed;
