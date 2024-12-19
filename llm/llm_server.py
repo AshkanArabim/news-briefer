d2b6c33539998a2e4dd5c53b7f12d99aaafeeb18
AshkanArabim <ashkan.arabim@gmail.com>
2024-12-19 11:44:10 -0700

    format llm_server.py

--- llm/llm_server.py
+++ llm/llm_server.py
@@ -13,30 +13,29 @@ model = None
 async def lifespan(app: FastAPI):
     # startup and shutdown events
     # see https://fastapi.tiangolo.com/advanced/events/
-    
+
     global model
-    
+
     # startup
     # get model name
-    model_name = os.environ.get('MODEL_NAME')
+    model_name = os.environ.get("MODEL_NAME")
     assert model_name is not None, "MODEL_NAME env variable not set!"
-    
-    
+
     if model_name.startswith("llama"):
         model = llm_classes.Llama(llama_name=model_name)
     elif model_name.startswith("gemini"):
-        gemini_model_name, api_key = model_name.split('|')
-        
+        gemini_model_name, api_key = model_name.split("|")
+
         model = llm_classes.Gemini2(model_name=gemini_model_name, api_key=api_key)
     else:
-        raise Exception('Invalid model name:', model_name)
-    
+        raise Exception("Invalid model name:", model_name)
+
     # app starts here
     yield
-    
+
     # shutdown
     # empty for now...
-    
+
 
 app = FastAPI(lifespan=lifespan)
 
@@ -46,15 +45,15 @@ def generate(prompt: str):
     def generate_stream():
         for chunk in model.generate(prompt):
             yield chunk
-    
+
     return StreamingResponse(generate_stream())
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     port = os.environ.get("TTS_PORT")
     assert port is not None, "TTS_PORT in tts-server is not set!"
     port = int(port)
-    
+
     is_dev = os.environ.get("IS_DEV", "false").lower() == "true"
-    
-    uvicorn.run(__name__ + ":app",host='0.0.0.0', port=port, reload=is_dev)
+
+    uvicorn.run(__name__ + ":app", host="0.0.0.0", port=port, reload=is_dev)
